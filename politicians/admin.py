666488034d7c2adc5e42433ceda64ff90ddbe03b
Mark Wong <mark@squarepotato.com>
2020-10-14 10:26:31 +0800

    add constituency and appointments

--- politicians/admin.py
+++ politicians/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Politician
+from .models import Politician, Constituency, Appointment
 # Register your models here.
 class PoliticianAdmin(admin.ModelAdmin):
     list_display = ('name', 'othername')
 admin.site.register(Politician, PoliticianAdmin)
+admin.site.register(Constituency)
+admin.site.register(Appointment)
--- politicians/models.py
+++ politicians/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Constituency(models.Model):
+    code = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
+    wikipedia = models.CharField(max_length=255, blank=True)
+    created = models.DateTimeField()
+    class Meta:
+        verbose_name_plural = "Constituencies"
+class Appointment(models.Model):
+    name = models.CharField(max_length=255)
+    othername = models.CharField(max_length=255, blank=True)
+    created = models.DateTimeField()
 class Politician(models.Model):
     name = models.CharField(max_length=255)
     firstname = models.CharField(max_length=255)
@@ -9,5 +20,7 @@ class Politician(models.Model):
     twitter = models.CharField(max_length=255, blank=True)
     facebook = models.CharField(max_length=255, blank=True)
     wikipedia = models.CharField(max_length=255, blank=True)
+    constituency = models.ManyToManyField(Constituency, blank=True)
+    appointment = models.ManyToManyField(Appointment, blank=True)
     def __str__(self):
         return self.name
