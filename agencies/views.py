d01118108b34dff466c1d1b131f459cea37c05c8
Mark Wong <mark@squarepotato.com>
2020-10-15 16:41:43 +0800

    add scrap button to agencies

--- agencies/views.py
+++ agencies/views.py
@@ -16,7 +16,8 @@ def scrap(request):
     for origin in origins:
         results = {}
         url = origins[origin]
-        response = requests.get(url)
+        headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+        response = requests.get(url, headers=headers)
         soup = BeautifulSoup(response.text, features="html.parser")
         articles = soup.find('div', {'id' : 'articlenum'})
         listings = articles.find_all('li', {'class' : 'listing'})
