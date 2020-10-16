e2658ab0538fd21ffe16cd0d37458414bbfdb7b4
Mark Wong <mark@squarepotato.com>
2020-10-16 23:44:45 +0800

    correct tab

--- agencies/views.py
+++ agencies/views.py
@@ -81,9 +81,9 @@ def aggregator(request):
                         if Agency.objects.filter(headline=entry.title, politician=Politician.objects.get(id=politician['id']), source=Source.objects.get(id=source['id']), guid=entry.guid).count() > 0:
                             continue
                         first_image_url = ""
-                            soup = BeautifulSoup(entry.content[0].value, features="html.parser")
-                            if soup.find('img'):
-                                first_image_url = soup.find('img')['src']
+                        soup = BeautifulSoup(entry.content[0].value, features="html.parser")
+                        if soup.find('img'):
+                            first_image_url = soup.find('img')['src']
                         agency = Agency.objects.create(headline=entry.title, source=Source.objects.get(id=source['id']), published=entry.published, link=entry.link, first_image_url=first_image_url, guid=entry.guid)
                         agency.save()
                         agency.politician.add(Politician.objects.get(id=politician['id']) )
