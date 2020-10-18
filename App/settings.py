05867c2bc6cba5705b29aa158e7a81ac7845af74
Mark Wong <mark@squarepotato.com>
2020-10-18 21:51:53 +0800

    add static root

--- App/settings.py
+++ App/settings.py
@@ -127,6 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = os.environ['EMAIL_HOST']
