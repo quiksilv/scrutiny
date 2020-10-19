249ec19d293a404fdf0c47056d73efb79214206b
Mark Wong <mark@squarepotato.com>
2020-10-19 23:28:53 +0800

    adding some static pages

--- App/urls.py
+++ App/urls.py
@@ -24,6 +24,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', TemplateView.as_view(template_name="index.html") ),
     path('about/', TemplateView.as_view(template_name="about.html") ),
+    path('privacy/', TemplateView.as_view(template_name="about.html") ),
     path('admin/', admin.site.urls),
     path('politicians/', include('politicians.urls') ),
     path('posts/', include('posts.urls') ),
