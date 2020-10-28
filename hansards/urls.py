46678a2117b0e6a170317662caadc7ec0e8101dc
Mark Wong <mark@squarepotato.com>
2020-10-28 11:55:20 +0800

    hansards list

--- hansards/urls.py
+++ hansards/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('view/<str:name>', views.view, name='view'),
 ]
--- hansards/views.py
+++ hansards/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from .models import Hansard, Paragraph
 
+def index(request):
+    hansards = Hansard.objects.all()
+    return render(request, 'hansards/index.html', {"hansards": hansards} )
 def view(request, name):
     hansard = Hansard.objects.get(name=name)
     paragraphs = Paragraph.objects.filter(hansard=hansard)
